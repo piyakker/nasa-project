cd2ef4fadc2f3a7e8c5bc1f577d8102dd20ef5ac
piyakker <ericlien86@gmail.com>
2022-12-31 18:27:39 +0800

    add docker

--- server/src/app.js
+++ server/src/app.js
@@ -9,13 +9,13 @@ const api = require('./routes/api')
 
 app.use(cors({
   origin: 'http://localhost:3000'
-}))
-app.use(morgan('combined'))
+}));
+app.use(morgan('combined'));
 
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/v1', api)
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
